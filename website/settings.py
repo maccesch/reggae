aeb356e02e4b711518327d5c30401533d24c6bcd
maccesch <maccesch@web.de>
2010-12-03 00:28:00 +0100

    initial install works

--- website/settings.py
+++ website/settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_PATH, 'db'),   # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -100,6 +100,7 @@ INSTALLED_APPS = (
     'django.contrib.comments',
     'django.contrib.sitemaps',
     
+    'mptt',
     'tagging',
     'zinnia',
     'django_bitly',
--- website/urls.py
+++ website/urls.py
@@ -1,11 +1,20 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, include
 
 from django.contrib import admin
 admin.autodiscover()
 
+from zinnia.sitemaps import EntrySitemap
+# from zinnia.sitemaps import CategorySitemap
+
 urlpatterns = patterns('',
-    # Example:
-    # (r'^website/', include('website.foo.urls')),
+
+    (r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', { 'sitemaps': {
+        'blog': EntrySitemap,
+#        'categories': CategorySitemap,
+    }}),
+
+    (r'^blog/', include('zinnia.urls')),
+    (r'^comments/', include('django.contrib.comments.urls')),
 
     (r'^admin/', include(admin.site.urls)),
 )
