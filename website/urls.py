b5621350a42d5c8d3ec0fe3ad69a043ed49ca586
maccesch <maccesch@web.de>
2010-12-04 17:45:42 +0100

    finished initial website

--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -18,3 +19,8 @@ urlpatterns = patterns('',
 
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True})
+    )
